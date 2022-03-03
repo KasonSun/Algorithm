996f8276717a0be56bde215312df0ec67a9bf5ca
kasonsun <kasonsun@foxmail.com>
2022-03-03 22:30:40 +0800

    代码随想录刷题记录2.0(2022.3.2补充排序)

--- src/sort/countSort.java
+++ src/sort/countSort.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
  *      所以可以直接把 arr[i] 放到它输出数组中的位置上。假设有5个数小于 arr[i]，所以 arr[i] 应该放在数组的第6个位置上。
  *
  *      待排序数组 int[] arr = {4,3,6,3,5,1};
- *      辅助计数数组 int[] help = new int[max - min + 1]; //该数组大小为待排序数组中的最大值减最小值+1
+ *      辅助计数数组 int[] bucket = new int[max - min + 1]; //该数组大小为待排序数组中的最大值减最小值+1
  *      输出数组 int[] res = new int[arr.length];
  *                1.求出待排序数组的最大值max=6， 最小值min=1
  *                2.实例化辅助计数数组help， help用来记录每个元素之前出现的元素个数
@@ -43,7 +43,7 @@ public class countSort {
             bucket[i] = bucket[i-1] + bucket[i];
         }
 
-        //3.根据help数组获取当前值在结果数组中下标索引
+        //3.根据bucket数组获取当前值在结果数组中下标索引
         int res[] = new int[nums.length];
         for(int i = 0; i < nums.length; i++){
             int index = --bucket[nums[i]];//当确定一个数以后，需要更新help数组--，操作都在这里实现了--bucket[nums[i]]（实现了索引index--和help[nums[i]]--）
